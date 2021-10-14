d878116f68559ac27312e505f72d05756cec95f4
EimantasBlazevicius <eima.blaz@gmail.com>
2021-10-14 22:59:01 +0300

    Ron Swonson API added, some layout functionality testing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Box, Text, SimpleGrid } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
+  const [qoute, setQoute] = useState<string>("");
+  const API_URL = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
+  useEffect(() => {
+    axios.get<string>(API_URL).then((response) => {
+      console.log(response.data[0]);
+      setQoute(response.data[0]);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box w="100%" p={2} color="black" bg="teal">
+        <Text as="em">{qoute}</Text>
+      </Box>
+      <Box w="100%" p={4}>
+        <SimpleGrid columns={2} spacing={5}>
+          <Box bg="tomato" height="80px"></Box>
+          <Box bg="tomato" height="80px"></Box>
+          <Box bg="tomato" height="80px"></Box>
+          <Box bg="tomato" height="80px"></Box>
+          <Box bg="tomato" height="80px"></Box>
+        </SimpleGrid>
+      </Box>
     </div>
   );
 }
